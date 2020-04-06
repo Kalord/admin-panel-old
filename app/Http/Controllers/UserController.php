bf7c0931f581fc56372ec20e2cfc25db27c1936c
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-04-06 19:11:53 +0300

    Added UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\CreateUserRequest;
+
+class UserController extends Controller
+{
+    /**
+     * Создание пользователя
+     * @param CreateUserRequest $request
+     */
+    public function create(CreateUserRequest $request)
+    {
+
+    }
+}
