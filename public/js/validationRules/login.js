0eb56aa232cd906bb0d334d08942897243fa2a79
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-04-07 03:06:24 +0800

    Refactoring

--- public/js/validationRules/login.js
+++ public/js/validationRules/login.js
@@ -6,7 +6,20 @@ const getValidationRulesLoginForm = () => {
         },
         '#password' : {
             stringValidator: [],
-            lengthValidator: [8, 30]
+            lengthValidator: [8, 255]
         }
     };
 };
+
+const getErrorMessageLoginForm = () => {
+    return {
+        '#login' : {
+            stringValidator: 'Некорректные данные',
+            lengthValidator: 'Поле должно быть больше 8 символов и меньше 30 символов'
+        },
+        '#password' : {
+            stringValidator: 'Некорректные данные',
+            lengthValidator: 'Поле должно быть больше 8 символов'
+        }
+    }
+}
--- public/js/validator.js
+++ public/js/validator.js
@@ -25,15 +25,67 @@ const integerValidator = (data) => {
     return typeValidator(data, 'integer');
 };
 
-const runValidation = (rules) => {
+const _errorMessages = {
+    lengthValidator:  'Uncorrect length',
+    emailValidator:   'Uncorrect email',
+    equalValidator:   'Not equal',
+    typeValidator:    'Type error',
+    stringValidator:  'Is not a string',
+    integerValidator: 'Is not a integer'
+};
+
+const getDefaultErrorMessage = (validator) => {
+    return _errorMessages[validator];
+};
+
+const getMessage = (key, validator, messages) => {
+    let messageList = messages[key];
+
+    if(!messageList || !messageList[validator]) return getDefaultErrorMessage(validator);
+
+    return messageList[validator];
+}
+
+const showErrorMessage = (validateElement, message, validator) => {
+    validateElement.css('border', '#a94442 solid 1px');
+    validateElement.after(`<span class="error-hint">${message}</span>`);
+};
+
+const hideErrorMessage = (validateElement) => {
+    validateElement.css('border', '');
+    let next = validateElement.next();
+    if(next.hasClass('error-hint')) next.remove();
+}
+
+const hideAllErrorMessages = () => {
+    $('.error-hint').remove();
+}
+
+const runValidation = (rules, messages = {}) => {
       for(let rule in rules) {
-          let data = $(rule);
-          if(!data.get(0)) continue;
+          let validateElement = $(rule);
+          if(!validateElement.get(0)) continue;
 
           let validators = rules[rule];
 
           for(let validator in validators) {
-              console.log(validator);
+              let params = validators[validator];
+              params.insert(0, `'${validateElement.val()}'`);
+              params = params.join(', ');
+
+              let resultValidation = eval(`${validator}(${params})`);
+
+              hideErrorMessage(validateElement);
+
+              if(!resultValidation)
+              {
+                  showErrorMessage(validateElement, getMessage(rule, validator, messages), validator);
+              }
           }
       }
+
+      let result = !$('.error-hint').get(0);
+      if(result) hideAllErrorMessages();
+
+      return result;
 };
