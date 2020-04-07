a0d34f8c63f0d71c0ed6425e2790e1c272994043
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-04-08 01:02:54 +0300

    Update

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Module;
 
 class SiteController extends Controller
 {
     public function index()
     {
-        return view('panel/home');
+        return view('panel/home', [
+            'modules' => Module::all()
+        ]);
     }
 
     public function login()
