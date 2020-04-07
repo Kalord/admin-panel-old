cf3853577763700dd58090d6878c51dab7e01e28
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-04-07 23:35:22 +0800

    Added function: getFirstProperty

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,9 @@
 Array.prototype.insert = function ( index, item ) {
     this.splice( index, 0, item );
 };
+
+const getFirstProperty = (object) => {
+    for (let i in object) {
+  		return object[i];
+  	}
+}
