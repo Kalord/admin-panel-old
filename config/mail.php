32b56dd19d710289954301d6aa99d3f0e58ef5d0
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-04-07 23:33:52 +0800

    Update

--- config/mail.php
+++ config/mail.php
@@ -49,7 +49,8 @@ return [
         ],
 
         'mailgun' => [
-            'transport' => 'mailgun',
+            'domain' => '',
+            'secret' => '',
         ],
 
         'postmark' => [
