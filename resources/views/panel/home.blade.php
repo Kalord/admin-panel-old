bdbff60af546239055314a1392b83b55ae78334f
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-04-08 00:19:34 +0300

    Added home view

---
+++ resources/views/panel/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('title', 'Home')
+@section('module', 'Home')
+@section('path', 'Home')
+
+@section('title', 'Авторизация')
+
+@section('content')
+    <h1>Добро пожаловать в панель администратора!</h1>
+@endsection
+
