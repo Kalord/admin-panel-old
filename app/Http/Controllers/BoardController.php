cf3d4cde6f9c78390bd8cac7d0d2103a676170c0
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-04-09 02:52:22 +0800

    Added logic for work with projects

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Module;
 use Illuminate\Http\Request;
 use App\Http\Modules\IModuleController;
+use App\Module;
+use App\Project;
 
 class BoardController extends Controller implements IModuleController
 {
     public function index()
     {
         return view('panel/board', [
-            'modules' => Module::all()
+            'modules' => Module::all(),
+            'projects' => Project::all()
         ]);
     }
 }
