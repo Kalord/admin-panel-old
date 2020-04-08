f33b94fd8331b1b8b84c5a0de0f94a3522e1cd18
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-04-09 02:55:22 +0800

    Added project.js

---
+++ public/js/project.js
@@ -0,0 +1,21 @@
+$('.project-item').click((event) => {
+    let projectItem = $(event.target);
+
+    while(!projectItem.attr('data-id'))
+    {
+        projectItem = projectItem.parent();
+    }
+
+    let id_selected = projectItem.attr('data-id');
+
+    const successCallback = (html) => {
+        $('.selected-project').removeClass('selected-project');
+        projectItem.toggleClass('selected-project');
+    };
+
+    const errorCallback = (html) => {
+        console.log(html);
+    }
+
+    changeProject(id_selected, successCallback, errorCallback);
+});
