77671e85c81ce0ed377bf7b1b2ec88ec65eb1954
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-04-12 02:59:06 +0800

    Added function: createProject

--- public/js/ajax/api/project.js
+++ public/js/ajax/api/project.js
@@ -8,6 +8,24 @@ const changeProject = (id_selected, successCallback, errorCallback) => {
         data: {
             id_selected: id_selected
         },
-        success: successCallback
+        success: successCallback,
+        error: errorCallback
     });
 };
+
+/**
+ * Создание проекта
+ */
+const createProject = (data, successCallback, errorCallback) => {
+    $.ajax({
+        type: POST,
+        url: `${PROJECT_API}/create`,
+        cache: false,
+        contentType: false,
+        processData: false,
+        async: false,
+        data: data,
+        success: successCallback,
+        error: errorCallback
+    });
+}
